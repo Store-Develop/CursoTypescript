c31620e9829b025f9357e951dc5135e1a220aada
JuanPerez <jkml563@gmail.com>
2022-06-25 16:37:48 -0500

    Definición de generics con TypeScript

---
+++ Full-TypeScript/Generics/generics.js
@@ -0,0 +1,7 @@
+"use strict";
+let arregloNumeros;
+arregloNumeros = [1, 2, 3, 4];
+//Esto nos marcara un error por que es de tipo string.
+//arregloNumeros = ['juan', 'Karla'];
+console.log(arregloNumeros);
+console.log(arregloNumeros[1]);
---
+++ Full-TypeScript/Generics/generics.ts
@@ -0,0 +1,8 @@
+let arregloNumeros:Array<number>;
+
+arregloNumeros = [1, 2, 3, 4] ;
+//Esto nos marcara un error por que es de tipo string.
+//arregloNumeros = ['juan', 'Karla'];
+
+console.log(arregloNumeros);
+console.log(arregloNumeros[1]);
